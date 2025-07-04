03c6a520833cc339e62881719c5c12ce7c63a7f0
seoaaa <seoaa3331@gmail.com>
2025-07-04 14:12:47 +0900

    ✨feat : 회원가입, 로그인, 프로필 수정, 회원 정보 조회, 회원 탈퇴 구현

--- src/main/java/com/example/demo/domain/user/dto/UserResponseDto/UserResponseDto.java
+++ src/main/java/com/example/demo/domain/user/dto/UserResponseDto/UserResponseDto.java
@@ -1,4 +1,47 @@
 package com.example.demo.domain.user.dto.UserResponseDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
 public class UserResponseDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Login {
+        private String accessToken;
+        private String refreshToken;
+        private String nickname;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class UserInfo {
+        private String loginId;
+        private String username;
+        private String nickname;
+        private boolean isDeleted;
+        private LocalDateTime createdAt;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class LikedPost {
+        private Long postId;
+        private String title;
+        private String content;
+        private String imageUrl;
+        private LocalDateTime likedAt;
+        private String communityName;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CommonResponse {
+        private String status;
+        private String message;
+        private Object data;
+    }
 }
+
