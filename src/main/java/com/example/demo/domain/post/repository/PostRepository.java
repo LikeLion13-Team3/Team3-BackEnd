5890dfed1745f0b322c0fb6fb0545e46c8b909e6
chw1230 <choihw1230@naver.com>
2025-07-11 20:36:12 +0900

    ✨ feat: 게시글 상세 조회 로직

--- src/main/java/com/example/demo/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/demo/domain/post/repository/PostRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByCommunityId(Long communityId, Pageable pageable);
+    Optional<Post> findByIdAndCommunityId(Long postId, Long communityId);
+
 }
